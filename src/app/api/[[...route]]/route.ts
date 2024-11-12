c869edd3169862947a065a95748310f9b8eccf9e
Iraildo Silva <iraildosilva1999@gmail.com>
2024-11-11 22:15:49 -0300

    feat: add hono

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+
+import hello from './hello'
+
+export const runtime = 'edge'
+
+const app = new Hono().basePath('/api')
+
+const routes = app.route('/hello', hello)
+
+export const GET = handle(app)
+export const POST = handle(app)
+export const PUT = handle(app)
+export const DELETE = handle(app)
+export type AppType = typeof routes
