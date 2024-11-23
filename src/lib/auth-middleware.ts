5f321579d6cf040ce9f4be20d098493180eb2751
Iraildo Silva <iraildosilva1999@gmail.com>
2024-11-22 21:24:33 -0300

    feat: create auth middleware

---
+++ src/lib/auth-middleware.ts
@@ -0,0 +1,41 @@
+'server-only'
+
+import { AUTH_COOKIE } from '@/features/auth/constants'
+import { getCookie } from 'hono/cookie'
+import { createMiddleware } from 'hono/factory'
+import { verify } from 'hono/jwt'
+import { env } from '@/lib/config'
+import prisma from './db'
+import { User } from '@prisma/client'
+
+type AdditionalContext = {
+  Variables: {
+    user: User
+  }
+}
+
+export const authMiddleware = createMiddleware<AdditionalContext>(
+  async (c, next) => {
+    const accessToken = getCookie(c, AUTH_COOKIE)
+
+    if (!accessToken) {
+      return c.json({ error: 'Unauthorized' }, 401)
+    }
+
+    const { sub: userId } = await verify(accessToken, env.JWT_SECRET)
+
+    console.log(userId)
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId as string },
+    })
+
+    if (!user) {
+      return c.json({ error: 'User not found' }, 404)
+    }
+
+    c.set('user', user)
+
+    await next()
+  }
+)
