b470c9e0f915bed91bec6778f0052a7f6ebfe800
Iraildo Silva <iraildosilva1999@gmail.com>
2024-11-22 21:29:05 -0300

    feat: create use-current api hook

---
+++ src/features/auth/api/use-current.ts
@@ -0,0 +1,22 @@
+import { client } from '@/lib/rpc'
+import { useQuery } from '@tanstack/react-query'
+import { QUERY_KEYS } from '../constants'
+
+export function useCurrent() {
+  const query = useQuery({
+    queryKey: QUERY_KEYS.current,
+    queryFn: async () => {
+      const response = await client.api.auth.current.$get()
+
+      if (!response.ok) {
+        return null
+      }
+
+      const { data } = await response.json()
+
+      return data
+    },
+  })
+
+  return query
+}
