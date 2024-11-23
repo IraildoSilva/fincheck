06842c9e7ffe5e4b761a55aec38efe4a29ee6071
Iraildo Silva <iraildosilva1999@gmail.com>
2024-11-23 16:17:47 -0300

    feat: add user button

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,21 +1,21 @@
 'use client'
 
 import { Logo } from '@/components/logo'
-import { ToggleTheme } from '@/components/toggle-theme'
-import { Button } from '@/components/ui/button'
-import { useLogout } from '@/features/auth/api/use-logout'
 import { BankAccounts } from '@/features/bank-accounts/components/bank-accounts'
+import { UserButton } from '@/components/user-button'
+import { useUserContext } from '@/hooks/use-user-context'
 
 export default function Dashboard() {
-  const { mutate: handleLogout } = useLogout()
+  const { user, isLoading } = useUserContext()
+
+  const avatar = user?.name.slice(0, 2).toUpperCase()
 
   return (
     <div className="h-full max-h-screen p-4 lg:px-8 lg:pt-6 flex flex-col gap-4">
       <header className="h-12 flex items-center justify-between">
         <Logo className="text-primary h-6" />
 
-        <ToggleTheme />
-        <Button onClick={() => handleLogout()}>Sair</Button>
+        <UserButton user={user} isLoading={isLoading} avatar={avatar} />
       </header>
 
       <main className="flex-1 flex flex-col lg:flex-row gap-4 lg:pr-4 max-h-[calc(100%-64px)]">
---
+++ src/components/user-button.tsx
@@ -0,0 +1,103 @@
+import { useTheme } from 'next-themes'
+
+import { Loader2, LogOutIcon } from 'lucide-react'
+
+import { User } from '@prisma/client'
+
+import { Avatar, AvatarFallback } from './ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import { useLogout } from '@/features/auth/api/use-logout'
+
+interface UserButtonProps {
+  isLoading: boolean
+  avatar?: string
+  user: Omit<User, 'password'>
+}
+
+export function UserButton({ isLoading, avatar, user }: UserButtonProps) {
+  const { setTheme } = useTheme()
+  const { mutate: handleLogout } = useLogout()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar>
+          <AvatarFallback className="cursor-pointer">
+            {!isLoading && avatar}
+            {isLoading && <Loader2 className="animate-spin size-5" />}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="max-w-60">
+        <DropdownMenuLabel className="px-5">
+          <div className="flex gap-4 items-center">
+            <Avatar className="bg-zinc-500/25 w-10 h-10 rounded-full flex items-center justify-center cursor-default">
+              <AvatarFallback className="font-medium text-sm tracking-[-0.5px] bg-transparent">
+                {avatar}
+              </AvatarFallback>
+            </Avatar>
+
+            <div>
+              <span className="block leading-4">{user?.name}</span>
+              <span className="text-xs text-muted-foreground">
+                {user?.email}
+              </span>
+            </div>
+          </div>
+        </DropdownMenuLabel>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem className="pl-10 pr-5 flex items-center justify-between ">
+          Perfil
+        </DropdownMenuItem>
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger className="pl-10 pr-5 hidden md:flex">
+            Tema
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent className="">
+              <DropdownMenuItem
+                className="flex items-center justify-between "
+                onClick={() => setTheme('light')}
+              >
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center justify-between "
+                onClick={() => setTheme('dark')}
+              >
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center justify-between "
+                onClick={() => setTheme('system')}
+              >
+                System
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className="flex items-center justify-between  pl-10 pr-5 "
+          onClick={() => handleLogout()}
+        >
+          <span>Sair</span>
+          <LogOutIcon className="w-4 h-4" />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
