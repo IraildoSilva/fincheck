667a03786b4f2fa5721f7f3f8a5daee1a8eaff24
Iraildo Silva <iraildosilva1999@gmail.com>
2024-12-04 22:36:52 -0300

    feat: use auth middleware in get bank accounts route

--- src/features/bank-accounts/server/route.ts
+++ src/features/bank-accounts/server/route.ts
@@ -1,32 +1,9 @@
 import { Hono } from 'hono'
 import prisma from '@/lib/db'
-import { getCookie } from 'hono/cookie'
-import { AUTH_COOKIE } from '@/features/auth/constants'
-import { verify } from 'hono/jwt'
-import { env } from '@/lib/config'
-import type { JwtPayload } from 'jsonwebtoken'
+import { authMiddleware } from '@/lib/auth-middleware'
 
-const app = new Hono().get('/', async (c) => {
-  const accessToken = getCookie(c, AUTH_COOKIE)
-
-  if (!accessToken) {
-    return c.json(
-      { error: 'Unauthorized', message: 'Missing Access Token' },
-      401
-    )
-  }
-
-  const { sub: userId } = (await verify(
-    accessToken,
-    env.JWT_SECRET
-  )) as JwtPayload
-
-  if (!userId) {
-    return c.json(
-      { error: 'Unauthorized', message: 'Invalid Access Token' },
-      401
-    )
-  }
+const app = new Hono().get('/', authMiddleware, async (c) => {
+  const userId = c.get('userId')
 
   const bankAccounts = await prisma.bankAccount.findMany({
     where: { userId },
