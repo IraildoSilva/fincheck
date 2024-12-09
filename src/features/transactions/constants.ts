28ebb307c820d329b8411aed77c84956738019ab
Iraildo Silva <iraildosilva1999@gmail.com>
2024-12-08 21:49:29 -0300

    feat: create month constants

--- src/features/transactions/constants.ts
+++ src/features/transactions/constants.ts
@@ -1,3 +1,33 @@
 export const QUERY_KEYS = {
   transactions: ['transactions'],
 }
+
+export const MONTHS = [
+  'Jan',
+  'Fev',
+  'Mar',
+  'Abr',
+  'Mai',
+  'Jun',
+  'Jul',
+  'Ago',
+  'Set',
+  'Out',
+  'Nov',
+  'Dez',
+]
+
+export const VerboseMonths = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro',
+]
