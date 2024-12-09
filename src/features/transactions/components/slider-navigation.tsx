f86fc0248188532508e39a553f706b0d1c944430
Iraildo Silva <iraildosilva1999@gmail.com>
2024-12-08 23:06:20 -0300

    feat: add filter transactions slider navigation

---
+++ src/features/transactions/components/slider-navigation.tsx
@@ -0,0 +1,34 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons'
+import { useSwiper } from 'swiper/react'
+
+interface SliderNavigationProps {
+  isBeginning: boolean
+  isEnd: boolean
+}
+
+export function SliderNavigation({
+  isBeginning,
+  isEnd,
+}: SliderNavigationProps) {
+  const swiper = useSwiper()
+
+  return (
+    <>
+      <button
+        disabled={isBeginning}
+        onClick={() => swiper.slidePrev()}
+        className="absolute left-0 top-1/2 -translate-y-1/2 z-10 w-12 h-12 flex items-center justify-center bg-gradient-to-r from-background to-transparent disabled:opacity-40"
+      >
+        <ChevronLeftIcon className="size-5 text-gray-800" />
+      </button>
+
+      <button
+        disabled={isEnd}
+        onClick={() => swiper.slideNext()}
+        className="absolute right-0 top-1/2 -translate-y-1/2 z-10 w-12 h-12 flex items-center justify-center bg-gradient-to-l from-background to-transparent disabled:opacity-40"
+      >
+        <ChevronRightIcon className="size-5 text-gray-800" />
+      </button>
+    </>
+  )
+}
