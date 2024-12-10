cb50f749d0c53146c0ba169d6dd962b83bda7687
Iraildo Silva <iraildosilva1999@gmail.com>
2024-12-10 10:46:53 -0300

    feat: adjust filters modal styles

--- src/features/transactions/components/filters-modal.tsx
+++ src/features/transactions/components/filters-modal.tsx
@@ -1,5 +1,6 @@
 import { ResponsiveModal } from '@/components/responsive-modal'
 import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { useGetBankAccounts } from '@/features/bank-accounts/api/use-get-bank-accounts'
 import { cn } from '@/lib/utils'
 import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react'
@@ -34,67 +35,76 @@ export function FiltersModal({ onClose, open, onApplyFilters }: FiltersModal) {
 
   return (
     <ResponsiveModal
+      dialogClassName={'max-w-[400px]'}
       open={open}
       onOpenChange={onClose}
       // title="Filtros"
     >
-      <div>
-        <span className="text-lg font-bold tracking-[-1px]">Conta</span>
+      <Card className="rounded-md border-none shadow-none">
+        <CardHeader className="">
+          <CardTitle className="text-lg font-bold self-center">Filtros</CardTitle>
+        </CardHeader>
 
-        <div className="space-y-2 mt-2">
-          {accounts?.map((bankAccount) => (
-            <button
-              key={bankAccount.id}
-              onClick={() => handleSelectBankAccount(bankAccount.id)}
-              className={cn(
-                'p-2 rounded-md w-full text-left hover:bg-accent duration-150 transition-colors ',
-                selectedBankAccountId === bankAccount.id && '!bg-gray-200'
-              )}
-            >
-              {bankAccount.name}
-            </button>
-          ))}
-        </div>
-      </div>
+        <CardContent>
+          <div>
+            <span className="text-lg font-bold tracking-[-1px]">Conta</span>
 
-      <div className="mt-10">
-        <span className="text-lg font-bold tracking-[-1px]">Ano</span>
-
-        <div className="mt-2 w-52 flex items-center justify-between">
-          <button
-            onClick={() => handleChangeYear(-1)}
-            className="w-12 h-12 flex items-center justify-center"
-          >
-            <ChevronLeftIcon className="w-6 h-6" />
-          </button>
-
-          <div className="flex-1 text-center">
-            <span className="text-sm font-medium tracking-[-0.5px]">
-              {selectedYear}
-            </span>
+            <div className="space-y-2 mt-2">
+              {accounts?.map((bankAccount) => (
+                <button
+                  key={bankAccount.id}
+                  onClick={() => handleSelectBankAccount(bankAccount.id)}
+                  className={cn(
+                    'p-2 rounded-md w-full text-left hover:bg-accent duration-150 transition-colors ',
+                    selectedBankAccountId === bankAccount.id && '!bg-gray-200'
+                  )}
+                >
+                  {bankAccount.name}
+                </button>
+              ))}
+            </div>
           </div>
 
-          <button
-            onClick={() => handleChangeYear(1)}
-            className="w-12 h-12 flex items-center justify-center"
-          >
-            <ChevronRightIcon className="w-6 h-6" />
-          </button>
-        </div>
-      </div>
+          <div className="mt-10">
+            <span className="text-lg font-bold tracking-[-1px]">Ano</span>
 
-      <Button
-        className="w-full mt-10"
-        onClick={() => {
-          onClose()
-          onApplyFilters({
-            bankAccountId: selectedBankAccountId ?? undefined,
-            year: selectedYear,
-          })
-        }}
-      >
-        Aplicar Filtros
-      </Button>
+            <div className="mt-2 w-52 flex items-center justify-between">
+              <button
+                onClick={() => handleChangeYear(-1)}
+                className="w-12 h-12 flex items-center justify-center"
+              >
+                <ChevronLeftIcon className="w-6 h-6" />
+              </button>
+
+              <div className="flex-1 text-center">
+                <span className="text-sm font-medium tracking-[-0.5px]">
+                  {selectedYear}
+                </span>
+              </div>
+
+              <button
+                onClick={() => handleChangeYear(1)}
+                className="w-12 h-12 flex items-center justify-center"
+              >
+                <ChevronRightIcon className="w-6 h-6" />
+              </button>
+            </div>
+          </div>
+
+          <Button
+            className="w-full mt-10"
+            onClick={() => {
+              onClose()
+              onApplyFilters({
+                bankAccountId: selectedBankAccountId ?? undefined,
+                year: selectedYear,
+              })
+            }}
+          >
+            Aplicar Filtros
+          </Button>
+        </CardContent>
+      </Card>
     </ResponsiveModal>
   )
 }
