0af82f5266dbfed7c438c77f7928dbe6d43fdbea
Iraildo Silva <iraildosilva1999@gmail.com>
2024-12-10 10:46:40 -0300

    feat: add responsive modal dialog class name prop

--- src/components/responsive-modal.tsx
+++ src/components/responsive-modal.tsx
@@ -2,24 +2,32 @@ import { useMedia } from 'react-use'
 
 import { Dialog, DialogContent } from '@/components/ui/dialog'
 import { Drawer, DrawerContent } from '@/components/ui/drawer'
+import { cn } from '@/lib/utils'
 
 interface ResponsiveModalProps {
   children: React.ReactNode
   open: boolean
   onOpenChange: (open: boolean) => void
+  dialogClassName: string
 }
 
 export function ResponsiveModal({
   children,
   onOpenChange,
   open,
+  dialogClassName,
 }: ResponsiveModalProps) {
   const isDesktop = useMedia('(min-width: 1024px)', true)
 
   if (isDesktop) {
     return (
       <Dialog open={open} onOpenChange={onOpenChange}>
-        <DialogContent className="w-full sm:max-w-lg p-0 border-none overflow-y-auto hide-scrollbar max-h-[85vh]">
+        <DialogContent
+          className={cn(
+            'w-full max-w-lg p-0 border-none overflow-y-auto hide-scrollbar max-h-[85vh]',
+            dialogClassName
+          )}
+        >
           {children}
         </DialogContent>
       </Dialog>
