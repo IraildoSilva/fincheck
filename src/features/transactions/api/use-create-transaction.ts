2e9fb2a19ab2ce79573a223e475ded78bc221d33
Iraildo Silva <iraildosilva1999@gmail.com>
2024-12-11 21:46:32 -0300

    feat: query invalidations and toasts

--- src/features/transactions/api/use-create-transaction.ts
+++ src/features/transactions/api/use-create-transaction.ts
@@ -1,6 +1,9 @@
 import { client } from '@/lib/rpc'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { InferRequestType, InferResponseType } from 'hono'
+import { QUERY_KEYS } from '../constants'
+import { QUERY_KEYS as ACCOUNT_QUERY_KEY } from '@/features/bank-accounts/constants'
+import { toast } from 'sonner'
 
 type Route = typeof client.api.transactions.$post
 
@@ -8,6 +11,8 @@ type ResponseType = InferResponseType<Route>
 type RequestType = InferRequestType<Route>
 
 export function useCreateTransaction() {
+  const queryClient = useQueryClient()
+
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async ({ json }) => {
       const response = await client.api.transactions.$post({ json })
@@ -19,6 +24,21 @@ export function useCreateTransaction() {
       const data = await response.json()
 
       return data
+    },
+    onSuccess: ({ data }) => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.transactions })
+      queryClient.invalidateQueries({
+        queryKey: ACCOUNT_QUERY_KEY.bankAccounts,
+      })
+
+      toast.success(
+        data.type === 'EXPENSE'
+          ? 'Despesa cadastrada com sucesso!'
+          : 'Receita cadastrada com sucesso!'
+      )
+    },
+    onError: () => {
+      toast.error('Falha ao criar transação')
     },
   })
 
