468870af7eb39bcf827989a0f2339108e7c4a60f
Iraildo Silva <iraildosilva1999@gmail.com>
2024-12-13 11:51:14 -0300

    feat: revert last changes

--- src/components/responsive-modal.tsx
+++ src/components/responsive-modal.tsx
@@ -3,7 +3,6 @@ import { useMedia } from 'react-use'
 import { Dialog, DialogContent } from '@/components/ui/dialog'
 import { Drawer, DrawerContent } from '@/components/ui/drawer'
 import { cn } from '@/lib/utils'
-import { useRef, useEffect } from 'react'
 
 interface ResponsiveModalProps {
   children: React.ReactNode
@@ -22,30 +21,6 @@ export function ResponsiveModal({
 }: ResponsiveModalProps) {
   const isDesktop = useMedia('(min-width: 1024px)', true)
 
-  const formContainerRef = useRef<HTMLDivElement | null>(null)
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (formContainerRef.current) {
-        formContainerRef.current.style.setProperty(
-          'bottom',
-          `env(safe-area-inset-bottom)`
-        )
-      }
-    }
-
-    if (window.visualViewport) {
-      window.visualViewport.addEventListener('resize', handleResize)
-      handleResize() // Initial call in case the keyboard is already open
-    }
-
-    return () => {
-      if (window.visualViewport) {
-        window.visualViewport.removeEventListener('resize', handleResize)
-      }
-    }
-  }, [])
-
   if (isDesktop) {
     return (
       <Dialog open={open} onOpenChange={onOpenChange}>
@@ -63,10 +38,7 @@ export function ResponsiveModal({
 
   return (
     <Drawer open={open} onOpenChange={onOpenChange}>
-      <DrawerContent
-        className={cn('outline-none', drawerClassname)}
-        ref={formContainerRef}
-      >
+      <DrawerContent className={cn('outline-none', drawerClassname)}>
         <div className="overflow-y-auto hide-scrollbar">{children}</div>
       </DrawerContent>
     </Drawer>
