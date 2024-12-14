aa0ff434aff7700c1ff4d71f2b88d06ac3932a79
Iraildo Silva <iraildosilva1999@gmail.com>
2024-12-14 20:13:34 -0300

    feat: add disabled status on input currency

--- src/components/input-currency.tsx
+++ src/components/input-currency.tsx
@@ -4,18 +4,20 @@ import { cn } from '@/lib/utils'
 interface InputCurrencyProps {
   onChange: (value: string) => void
   value?: string | number
+  disabled?: boolean
 }
 
-export function InputCurrency({ onChange, value }: InputCurrencyProps) {
+export function InputCurrency({ onChange, value, disabled }: InputCurrencyProps) {
   return (
     <div>
       <NumericFormat
+        disabled={disabled}
         decimalSeparator=","
         thousandSeparator="."
         defaultValue={value}
         onChange={(event) => onChange(event.target.value)}
         className={cn(
-          'w-full text-[32px] bg-transparent font-bold tracking-[-1px] outline-none'
+          'w-full text-[32px] bg-transparent font-bold tracking-[-1px] outline-none disabled:cursor-not-allowed disabled:text-muted-foreground'
         )}
       />
     </div>
