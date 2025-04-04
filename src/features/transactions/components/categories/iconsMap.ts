34d5aa5d0c5d60bd355126533680c0533c009d59
Iraildo Silva <iraildosilva1999@gmail.com>
2025-04-04 11:56:12 -0300

    feat: add shopee income icon variant

--- src/features/transactions/components/categories/iconsMap.ts
+++ src/features/transactions/components/categories/iconsMap.ts
@@ -1,17 +1,19 @@
-import { Clothes } from "./expense/clothes";
-import { Education } from "./expense/education";
-import { Expense } from "./expense/expense";
-import { Food } from "./expense/food";
-import { Fun } from "./expense/fun";
-import { Grocery } from "./expense/grocery";
-import { Home } from "./expense/home";
-import { Transport } from "./expense/transport";
-import { Travel } from "./expense/travel";
-import { Income } from "./income/income";
+import { Clothes } from './expense/clothes'
+import { Education } from './expense/education'
+import { Expense } from './expense/expense'
+import { Food } from './expense/food'
+import { Fun } from './expense/fun'
+import { Grocery } from './expense/grocery'
+import { Home } from './expense/home'
+import { Transport } from './expense/transport'
+import { Travel } from './expense/travel'
+import { Income } from './income/income'
+import { Shopee } from './income/shopee'
 
 export const iconsMap = {
   income: {
     default: Income,
+    shopee: Shopee,
   },
   expense: {
     default: Expense,
@@ -24,4 +26,4 @@ export const iconsMap = {
     transport: Transport,
     travel: Travel,
   },
-};
+}
